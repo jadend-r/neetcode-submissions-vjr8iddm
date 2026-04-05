a867b818a2227d03aaa17ee5049aab4f9c222b56
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-04-05 03:08:49 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(curr, remaining):
+            if not remaining:
+                res.append(curr.copy())
+                return
+
+            for i in range(len(remaining)):
+                curr.append(remaining[i])
+                dfs(curr, remaining[:i] + remaining[i + 1:])
+                curr.pop()
+        dfs([], nums)
+        return res
