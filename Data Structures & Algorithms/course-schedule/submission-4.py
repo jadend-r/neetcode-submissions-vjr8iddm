0dce5be3fb14f79776c40f24ee99755456746200
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-04-14 13:58:35 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = collections.defaultdict(list)
+        visited = set()
+        # build the adjacency list
+        for course, prereq in prerequisites:
+            prereqs[course].append(prereq)
+
+        def dfs(course):
+            if course in visited: #Cycle exists, cant complete
+                return False
+            if prereqs[course] == []: #No prereqs, course can complete
+                return True
+            
+            visited.add(course)
+            for prereq in prereqs[course]:
+                if not dfs(prereq):
+                    return False
+            prereqs[course] = [] #mark course as complete 
+            visited.remove(course)
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+
+            
