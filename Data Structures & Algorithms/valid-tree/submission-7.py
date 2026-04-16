43f80212b2ef0a91f7f6a8723b4d2043796ff6df
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-04-15 21:14:01 -0400

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not n:
+            return True
+        adj = collections.defaultdict(list)
+        visited = set()
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        def dfs(node, parent=None):
+            if node in visited:
+                return False
+
+            visited.add(node)
+            for adjacent in adj[node]:
+                if adjacent == parent:
+                    continue
+                if not dfs(adjacent, node):
+                    return False
+
+            return True
+
+        if dfs(0) and len(visited) == n:
+            return True
+
+        return False
