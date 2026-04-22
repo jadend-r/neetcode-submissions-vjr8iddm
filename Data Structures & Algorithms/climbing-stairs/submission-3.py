96c80d78430f71cb9183af2f762ebb2cb8dacba4
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-04-22 01:34:28 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,6 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev1, prev2 = 1, 0
+        for i in range(n):
+            prev2, prev1 = prev1, prev1 + prev2
+        return prev1
