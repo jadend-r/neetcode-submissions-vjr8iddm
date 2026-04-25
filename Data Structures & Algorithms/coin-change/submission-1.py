1344ad921672b65b2c11f18d1016aed2c012767f
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-04-25 03:09:51 -0400

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = amount
+        if n == 0:
+            return 0
+        dp = [math.inf] * (n + 1)
+        #dp[i] = fewest num of coins to make amount i
+        #dp[0] = 0
+        #dp[1] = 1
+        dp[0] = 0
+
+        for amt in range(amount + 1):
+            for coin in coins:
+                if amt - coin < 0:
+                    continue
+                dp[amt] = min(dp[amt], dp[amt-coin] + 1)
+
+        return dp[n] if dp[n] != math.inf else -1
