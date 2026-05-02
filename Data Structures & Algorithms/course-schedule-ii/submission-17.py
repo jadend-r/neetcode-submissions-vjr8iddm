5774cab0d83aa0d7d2e204ca6daaa4062e1a5f2a
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-02 02:32:09 -0400

    Add: course-schedule-ii - submission-17

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-17.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = collections.defaultdict(list)
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
+
+        res = []
+        visited = set()
+        added = set()
+
+        def dfs(course):
+            if course in visited:
+                print("ret false", course)
+                return False
+
+            if adj[course] == [] and course in added:
+                return True
+
+            visited.add(course)
+            for prereq in adj[course]:
+                if not dfs(prereq):
+                    print("ret false", course, prereq)
+                    return False
+            visited.remove(course)
+            adj[course] = []
+            res.append(course)
+            added.add(course)
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                print("ret false ok", course)
+                return []
+
+        print("yuh")
+        return res
