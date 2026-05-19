6625814b5d6c2034ca978f9cc54b39d2c5d11479
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-19 00:33:10 -0400

    Add: valid-tree - submission-17

---
+++ Data Structures & Algorithms/valid-tree/submission-17.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        # depth first search cycle detection
+        # build adj list 
+        adj = collections.defaultdict(list)
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        visited = set()
+        def dfs(node, parent):
+            if node in visited:
+                return False #cycle detected, not a valid tree
+            
+            visited.add(node)
+            for adjacent in adj[node]:
+                if adjacent == parent:
+                    continue
+                if not dfs(adjacent, node):
+                    return False
+            return True
+        return dfs(0, -1) and len(visited) == n
+
+            
