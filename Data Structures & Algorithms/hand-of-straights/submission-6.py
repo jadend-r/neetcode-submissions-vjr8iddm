5d757ae112cd87598535c6f7a5c9b4c904b87ce1
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-20 01:52:00 -0400

    Add: hand-of-straights - submission-6

---
+++ Data Structures & Algorithms/hand-of-straights/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = {}
+        # count each card that we have O(n)
+        for c in hand:
+            count[c] = 1 + count.get(c, 0)
+        # minHeap to give us the smallest card in hand
+        minHeap = list(count.keys())
+        heapq.heapify(minHeap)
+        # try to form the group of size k starting from the smallest
+        while minHeap:
+            smallest = minHeap[0]
+
+            for c in range(smallest, smallest + groupSize):
+                if c not in count:
+                    return False
+                count[c] -= 1
+                if count[c] == 0:
+                    if c != minHeap[0]:
+                        return False
+                    heapq.heappop(minHeap)
+        return True
