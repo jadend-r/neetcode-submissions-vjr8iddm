4d75948662793f92646cbefbfaa544f9cc0049eb
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-21 01:40:06 -0400

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #graph cycle detection problem
+        #view our prereqs as directed graph w/ edge from course -> preq
+        #if a cycle exists in this graph, impossible to finish all courses
+
+        adj = defaultdict(list)
+        visited = set()
+        for course, prereq in prerequisites:
+            adj[course].append(prereq)
+
+        def dfs(course):
+            if course in visited:
+                return False
+
+            if adj[course] == []:
+                return True
+
+            visited.add(course)
+            for prereq in adj[course]:
+                if not dfs(prereq):
+                    return False
+            visited.remove(course)
+            adj[course] == []
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
+            
