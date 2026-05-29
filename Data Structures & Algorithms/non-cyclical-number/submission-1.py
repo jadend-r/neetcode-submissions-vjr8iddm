5d89ca0e7b462b151c93da4617fd9d93a7a8b8db
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-29 01:15:52 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        # use a set to store computed sum of squares
+        # if a sum of squares is seen again, return false
+        
+        #O(n) memory for seen set
+        seen = set()
+        while n != 1:
+            s = str(n)
+            summ = 0
+            for i in range(len(s)):
+                digit = int(s[i])
+                summ += digit**2
+            if summ in seen:
+                return False
+            seen.add(summ)
+            n = summ
+        return True
