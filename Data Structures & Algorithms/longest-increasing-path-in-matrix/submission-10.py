7fd0e485ad160fcd03bc9658e2693f038b25d60d
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-05-30 02:17:52 -0400

    Add: longest-increasing-path-in-matrix - submission-10

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-10.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        #brute force approach try every position as a start
+            #dfs in all directions where new val > prev
+            #O(m*n)^2
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        #add memo to dfs O(m*n) time & space
+        memo = {}
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        longest = 0
+        def dfs(r, c, prev):
+            if (
+                r < 0 or r >= ROWS or
+                c < 0 or c >= COLS or
+                matrix[r][c] <= prev
+            ):
+                return 0
+
+            if (r, c) in memo:
+                return memo[(r, c)]
+
+            #get the LIS from all 4 neighbors and add 1
+            lis = 0
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                lis = max(lis, dfs(nr, nc, matrix[r][c]))
+            memo[(r, c)] = 1 + lis
+            return memo[(r, c)]
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                longest = max(longest, dfs(r, c, -1))
+        return longest
+            
