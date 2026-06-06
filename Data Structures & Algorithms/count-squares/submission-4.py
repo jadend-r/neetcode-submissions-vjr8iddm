cf4093f60c19d3a4bd814860e142cd800b40b290
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-06-06 02:19:54 -0400

    Add: count-squares - submission-4

---
+++ Data Structures & Algorithms/count-squares/submission-4.py
@@ -0,0 +1,20 @@
+class CountSquares:
+
+    def __init__(self):
+        self.pointCount = defaultdict(int)
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        self.pointCount[tuple(point)] += 1
+        self.points.append(point)
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+        px, py = point
+        for x, y in self.points:
+            if abs(px - x) != abs(py - y) or px == x or py == y:
+                continue
+            if [x, py] not in self.points or [px, y] not in self.points:
+                continue
+            res += self.pointCount[(x, py)] * self.pointCount[(px, y)]
+        return res
