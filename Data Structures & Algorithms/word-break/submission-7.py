0fc230632f08b63f620b6efb4bc0fc4f9aa9bd40
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-07-24 02:12:22 -0400

    Add: word-break - submission-7

---
+++ Data Structures & Algorithms/word-break/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        #brute force is compute every possible way to split s into
+            #max(len(wordDict)) segments
+                #check all segments in wordDict
+                #O(n * len(wordDict))
+        #optimize to O(n) utilizing dp
+        #ptr segmenting s into two halves 
+            #dp[i] = :i in wordDict and j: segmentable 
+
+        wordDict = set(wordDict)
+        n = len(s) 
+        dp = [False] * (n + 1)
+        dp[n] = True #dp[i] = can s[i:] be broken into dict words
+                            #s[len(s)] is empty so dp[len(s)] = True
+                                #can be broken into 0 words
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n + 1):
+                if dp[j] and s[i:j] in wordDict:
+                    dp[i] = True
+                    break
+        return dp[0]
