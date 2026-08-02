6bb0cdbd9fbf3b53e8d7daa70c8330f1921d549c
Jaden Reid <40547891+jadend-r@users.noreply.github.com>
2026-08-02 16:41:11 -0400

    Add: word-break - submission-20

---
+++ Data Structures & Algorithms/word-break/submission-20.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        wordDict = set(wordDict)
+        memo = defaultdict(bool)
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i == len(s):
+                return True
+            L = max(map(len, wordDict))
+            for j in range(i + 1, min(len(s) + 1, i + 1 + L)):
+                if s[i:j] in wordDict and dfs(j):
+                    memo[i] = True
+            return memo[i]
+        return dfs(0)
